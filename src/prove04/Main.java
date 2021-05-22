0d5f9153a4b6a5b775dd4b4e03f4419d1e4a3153
GalvezGMCollaboration <84644127+GalvezGMCollaboration@users.noreply.github.com>
2021-05-23 05:49:51 +0800

    Teammate Attempt 4
    Prove 4 Requirement

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -4,6 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is George.");
-        System.out.println("Hello George, my name is George too.");
+        
+        System.out.println("Hello George, my name is George also.");
     }
 }
