fb701c34df96ebb22bc27bd343dc62e790ebfd0e
GalvezGMCollaboration <84644127+GalvezGMCollaboration@users.noreply.github.com>
2021-05-23 05:39:35 +0800

    Update Main.java
    Teammate Commit Attempt 2

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -4,6 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is George.");
-        System.out.println("Hello George, my name is George too.");
+        System.out.println("Hello George, my name is George.");
     }
 }
