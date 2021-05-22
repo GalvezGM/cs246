ab6c6179b416d479712ae23c4ad015a9af5c3339
GalvezGM <63634928+GalvezGM@users.noreply.github.com>
2021-05-23 05:17:31 +0800

    Teammate Commit Attempt

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -2,6 +2,8 @@ package prove04;
 
 class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is George.");
+        System.out.println("Hello George, my name is George too.");
     }
 }
