70cb6485cd82f9a56ab54bafd5e6ad1eec578060
GalvezGM <63634928+GalvezGM@users.noreply.github.com>
2021-05-23 05:19:41 +0800

    Revert "Teammate Commit Attempt"
    This reverts commit ab6c6179

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -2,8 +2,6 @@ package prove04;
 
 class HelloWorld {
     public static void main(String[] args) {
-
         System.out.println("Hello, my name is George.");
-        System.out.println("Hello George, my name is George too.");
     }
 }
